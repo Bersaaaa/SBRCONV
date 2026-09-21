b3faa0c7da8177a0400d943d5c362d879d3633a3
Bersaaaa <49647217+Bersaaaa@users.noreply.github.com>
2026-09-21 14:09:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // À compléter avec les identifiants de TON nouveau projet Supabase
 // (Dashboard Supabase > Project Settings > API)
 // ============================================================
-const SUPABASE_URL = "https://xxxxxxxxxxxx.supabase.co";
-const SUPABASE_ANON_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const SUPABASE_URL = "https://mqqcysuvxuagfxpbbthb.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_4eekdU225iv-T1p25GW3DA_M5OeDGla";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
